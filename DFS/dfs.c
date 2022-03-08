e0e249a8e7ce4e8fc83c03c801ff3d915de391dc
Denny Thomas <32785817+smash-cracker@users.noreply.github.com>
2022-03-08 22:59:50 +0530

    Add files via upload

---
+++ DFS/dfs.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+int visited[10];
+int a[10][10] = {
+    {0,1,1,1,1,0,0,0},
+    {1,0,0,0,0,1,0,0},
+    {1,0,0,0,0,1,0,0},
+    {1,0,0,0,0,0,1,0},
+    {1,0,0,0,0,0,1,0},
+    {0,1,1,0,0,0,0,1},
+    {0,0,0,1,1,0,0,1},
+    {0,0,0,0,0,1,1,0}
+};
+
+void DFS(int i) {
+    int j;
+    printf("%d ",i);
+    visited[i]=1;
+    for(int j=0; j<9;j++) {
+        if(a[i][j]==1 && !visited[j]) {
+            DFS(j);
+        }
+    }
+}
+
+int main() {
+    DFS(0);
+ }
+// void DFS(int);
+// void main()
+// {
+//     DFS(0);
+// }
+// void DFS(int i)
+// {
+//     int j;
+// printf("\n%d",i);
+//     visited[i]=1;
+// for(j=0;j<9;j++)
+//        if(!visited[j]&&a[i][j]==1)
+//             DFS(j);
+// }
